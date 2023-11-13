df74129ae7d04acd55c322f3a4756ec68ece2c1e
Mateus Dantas de Paula <mateus.dantas@senckenberg.de>
2023-11-13 09:04:50 +0100

    - rpc div by wtot

--- modules/growth.cpp
+++ modules/growth.cpp
@@ -1161,8 +1161,10 @@ bool allometry(Individual& indiv) {
 				//indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / (3.75e6 * 50.0);
 				//indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / (3.75e6 * 50.0);
 				// root surface area divided by soil surface area. This is calculated by ssa = 50 m2/g * soil dens, planar = 1.5e4 g/m2 / patcharea  = 1000 m. 
-				indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / (1.5e4 * 1e-3 * 50.0);
-				indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / (1.5e4 * 1e-3 * 50.0);
+				/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / (1.5e4 * 1e-3 * 50.0);
+				indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / (1.5e4 * 1e-3 * 50.0);*/ 
+				indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / indiv.vegetation.patch.soil.soiltype.wtot;
+				indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / indiv.vegetation.patch.soil.soiltype.wtot;
 
 				/*indiv.rpc /= 2.0;
 				indiv.rpc_myco /= 2.0;*/
@@ -1215,8 +1217,10 @@ bool allometry(Individual& indiv) {
 					indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9 * indiv.densindiv;*/
 					/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / (3.75e6 * 50.0);
 					indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / (3.75e6 * 50.0);*/
-					indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / (1.5e4 * 1e-3 * 50.0);
-					indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / (1.5e4 * 1e-3 * 50.0);
+					/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / (1.5e4 * 1e-3 * 50.0);
+					indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / (1.5e4 * 1e-3 * 50.0);*/
+					indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / indiv.vegetation.patch.soil.soiltype.wtot;
+					indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / indiv.vegetation.patch.soil.soiltype.wtot;
 
 					/*indiv.rpc /= 2.0;
 					indiv.rpc_myco /= 2.0;*/
