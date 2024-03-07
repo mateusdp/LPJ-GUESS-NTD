7fbe54de02cdd77d73287001b4ab284acada11d0
Mateus Dantas de Paula <mateus.dantas@senckenberg.de>
2024-03-07 09:52:17 +0100

    - deciduous turnover either 0 or 1 depending on phenological state

--- modules/growth.cpp
+++ modules/growth.cpp
@@ -2400,10 +2400,17 @@ void growth_natural_daily(Stand& stand, Patch& patch) {
 						turnover_sap = indiv.pft.turnover_sap / 365.0;
 					//}
 
-						if (indiv.pft.phenology == RAINGREEN || indiv.pft.phenology == SUMMERGREEN) {
-							turnover_leaf *= 1 - indiv.phen;
-							turnover_root *= 1 - indiv.phen;
-							turnover_sap *= 1 - indiv.phen;
+						if ((indiv.pft.phenology == RAINGREEN || indiv.pft.phenology == SUMMERGREEN)) {
+							if (indiv.phen == 0) {
+								turnover_leaf = 1.0;
+								turnover_root = 1.0;
+								turnover_sap = 1.0;
+							}
+							else {
+								turnover_leaf = 0.0;
+								turnover_root = 0.0;
+								turnover_sap = 0.0;
+							}
 						}
 
 					// Tissue turnover with both nitrogen and phosphorus and associated litter production
