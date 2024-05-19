58ecf4f7d21e5dbca8003ba52c636c29a9d6c26c
Mateus Dantas de Paula <mateus.dantas@senckenberg.de>
2024-05-19 22:11:04 +0200

    - max water uptake limited by area

--- modules/growth.cpp
+++ modules/growth.cpp
@@ -1153,8 +1153,8 @@ bool allometry(Individual& indiv) {
 
 			// Root projective cover calculation, if SRL variation is activated.
 			if (ifsrlvary) {
-				indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI;
-				indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9;
+				/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI;
+				indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9;*/
 				/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / indiv.densindiv / patcharea;
 				indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9 / indiv.densindiv / patcharea;*/
 				/*indiv.rpc = indiv.srl * indiv.d_root * PI  * indiv.cmass_root * (1.0 - lambertbeer(indiv.srl * indiv.d_root * PI  * indiv.cmass_root));
@@ -1170,8 +1170,8 @@ bool allometry(Individual& indiv) {
 				// Water uptake based on root surface area (Khare 2022) (12 hs uptake), divided by soil water content
 				/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI * 61.56 / indiv.vegetation.patch.soil.soiltype.wtot / 1.5;
 				indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI * 61.56 / indiv.vegetation.patch.soil.soiltype.wtot / 1.5;*/
-				/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI / indiv.densindiv / water_content;
-				indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI / indiv.densindiv / water_content;*/
+				indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI * 61.56;
+				indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI * 61.56;
 
 				/*indiv.rpc /= 2.0;
 				indiv.rpc_myco /= 2.0;*/
@@ -1220,8 +1220,10 @@ bool allometry(Individual& indiv) {
 					indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9 / patcharea;*/
 					/*indiv.rpc = 1.0 - lambertbeer(indiv.srl * indiv.d_root * PI  * indiv.cmass_root);
 					indiv.rpc_myco = 1.0 - lambertbeer(indiv.cmass_myco * 2.0e-6 * PI * 2.55e9);*/
-					indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI;
-					indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9;
+					/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI;
+					indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9;*/
+					indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI * 61.56;
+					indiv.rpc_myco = indiv.cmass_myco * 2.6e9 * 2e-6 * PI * 61.56;
 					/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI * indiv.densindiv;
 					indiv.rpc_myco = indiv.cmass_myco * 2.0e-6 * PI * 2.55e9 * indiv.densindiv;*/
 					/*indiv.rpc = indiv.srl * indiv.d_root * indiv.cmass_root * PI * patcharea / (3.75e6 * 50.0);
